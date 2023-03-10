fd693030eff9aae1b5103efce7aa15a4db0463bd
Adrian Marculescu <adrianmarculescu.ing@gmail.com>
2023-03-10 11:19:47 +0100

    Star rating value recovery and insertion value into comment

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 // Select all the elements with the "i" tag and store them in a NodeList called "stars"
 const stars = document.querySelectorAll('.stars i')
 // Loop trough the "stars" NodeList
-console.log(stars)
+// console.log(stars)
+let curentRating=document.querySelector('.currentRating')
+let curentStarLevel=0;
 stars.forEach((star,index1) => {
   // Add an event listener that runs when the click "event" is triggered 
-  console.log(index1)
+  // console.log(index1)
   star.addEventListener('click',() =>{
+    // console.log('clicked')
+    // console.log(index1)
+    
+    curentStarLevel=index1+1;
+    curentRating.innerText=`${curentStarLevel}`;
+
     // Loop through the "stars" NodeList Again
     stars.forEach((star,index2) => {
-    //   console.log(index2)
+      // console.log(index2)
       //   Add the "active" class to the clicked star and any stars with a lower index 
       // and remove the "active" class form any stars with a higher index
       index1 >= index2 ? star.classList.add('active') : star.classList.remove('active')
@@ -25,9 +33,10 @@ submitButton.addEventListener('click', () => {
   // 1 get the values
   let inputForm = document.getElementById('form-textarea').value;
   let messageError=document.getElementById('message-error');
-  console.log(inputForm)
-
-  if(inputForm ===''){
+  let starLevel=curentRating;
+  // console.log(inputForm)
+  console.log(starLevel);
+  if(inputForm ===''&&curentStarLevel===0){
     return messageError.innerHTML = 'Le champ doit être rempli'
   } else {
     messageError.innerHTML='';
@@ -35,9 +44,11 @@ submitButton.addEventListener('click', () => {
   // 2 create elements
   let commentArea=document.createElement('div');
   let commentValue=document.createElement('p');
-  commentValue.innerHTML=inputForm;
-  let curentRating=
-
+  commentValue.innerHTML=`&#11088 ${curentStarLevel}/5 <br> ${inputForm}`;
+  console.log(curentRating)
+  // let curentRating=document.createElement('div')
+  // curentRating.innerText=`${curentRating}`;
+  
   // 3 add a class
   commentArea.classList.add('comments-area');
   
