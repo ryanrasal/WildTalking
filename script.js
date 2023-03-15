a1b07c07e3b52c9c1bc5b95f891bf77467585c1d
Raemay <[melaniebelliard@outlook.fr](mailto:melaniebelliard@outlook.fr)>
2023-03-15 14:12:46 +0100

    finished some details

--- script.js
+++ script.js
@@ -3,20 +3,20 @@
 const stars = document.querySelectorAll('.stars i')
 // Loop trough the "stars" NodeList
 // console.log(stars)
-let curentRating=document.querySelector('.currentRating')
-let curentStarLevel=0;
-stars.forEach((star,index1) => {
+let curentRating = document.querySelector('.currentRating')
+let curentStarLevel = 0;
+stars.forEach((star, index1) => {
   // Add an event listener that runs when the click "event" is triggered 
   // console.log(index1)
-  star.addEventListener('click',() =>{
+  star.addEventListener('click', () => {
     // console.log('clicked')
     // console.log(index1)
-    
-    curentStarLevel=index1+1;
-    curentRating.innerText=`${curentStarLevel}`;
+
+    curentStarLevel = index1 + 1;
+    curentRating.innerText = `${curentStarLevel}`;
 
     // Loop through the "stars" NodeList Again
-    stars.forEach((star,index2) => {
+    stars.forEach((star, index2) => {
       // console.log(index2)
       //   Add the "active" class to the clicked star and any stars with a lower index 
       // and remove the "active" class form any stars with a higher index
@@ -32,26 +32,26 @@ let formArea = document.getElementById('form-area');
 submitButton.addEventListener('click', () => {
   // 1 get the values
   let inputForm = document.getElementById('form-textarea').value;
-  let messageError=document.getElementById('message-error');
-  let starLevel=curentRating;
+  let messageError = document.getElementById('message-error');
+  let starLevel = curentRating;
   // console.log(inputForm)
   console.log(starLevel);
-  if(inputForm ===''&&curentStarLevel===0){
+  if (inputForm === '' && curentStarLevel === 0) {
     return messageError.innerHTML = 'Le champ doit être rempli'
   } else {
-    messageError.innerHTML='';
+    messageError.innerHTML = '';
   }
   // 2 create elements
-  let commentArea=document.createElement('div');
-  let commentValue=document.createElement('p');
-  commentValue.innerHTML=`&#11088 ${curentStarLevel}/5 <br> ${inputForm}`;
+  let commentArea = document.createElement('div');
+  let commentValue = document.createElement('p');
+  commentValue.innerHTML = `&#11088 ${curentStarLevel}/5 <br> ${inputForm}`;
   console.log(curentRating)
   // let curentRating=document.createElement('div')
   // curentRating.innerText=`${curentRating}`;
-  
+
   // 3 add a class
   commentArea.classList.add('comments-area');
-  
+
 
   // 4 specify the dependency of the sections
   formArea.appendChild(commentArea).appendChild(commentValue);
@@ -72,7 +72,7 @@ window.addEventListener('scroll', () => {
 //  Entre temps, si on scroll encore, mais que le temps n'est pas encore imparti
 //  (donc la navbar possède toujours la classe nav-opacity), on clear le timeout pour l'empêcher
 //  de supprimer la classe, et on le relance.
- 
+
 // FORMULAIRE
 
 // Récupérer l'élément HTML pour le champ de date
@@ -85,7 +85,7 @@ dateInput.addEventListener('change', () => {
 
   // Inverser la date sélectionnée
   const reversedDate = selectedDate.split('-').reverse().join('/');
-  
+
   // Afficher la date sélectionnée dans un élément HTML
   const result = document.getElementById('result');
   result.innerHTML = `Vous avez sélectionné la date : ${reversedDate}`;
@@ -99,26 +99,32 @@ document.getElementById('formulaire').addEventListener('submit', (e) => {
       erreur = 'Veuillez renseigner tous les champs';
     }
     if (erreur) {
-        e.preventDefault();
-        document.getElementById('erreur').innerHTML = erreur;
-        return false;
+      e.preventDefault();
+      document.getElementById('erreur').innerHTML = erreur;
+      return false;
     } else {
-        e.preventDefault();
-        alert("Message envoyé !");
+      e.preventDefault();
+      alert("Message envoyé !");
     }
-}
+  }
 });
 
 
-window.addEventListener('scroll', function() {
+
+const divAnimation = document.getElementById('strengths');
+
+window.addEventListener('scroll', function () {
   // code pour récupérer la position de défilement (scroll) de l'utilisateur
-  let scrollPosition = window.scrollY;
-  console.log(scrollPosition)
-  if (scrollPosition > 2000) {
+  if (window.scrollY > divAnimation.getBoundingClientRect().bottom) {
     // code pour ajouter une classe à une div
+
     document.getElementById('animate1').classList.add('strengths2');
     document.getElementById('animate2').classList.add('strengths3');
     document.getElementById('animate3').classList.add('strengths4-5');
-  } 
+  } else {
+    document.getElementById('animate1').classList.remove('strengths2');
+    document.getElementById('animate2').classList.remove('strengths3');
+    document.getElementById('animate3').classList.remove('strengths4-5');
+  }
 });
 
